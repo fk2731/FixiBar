97ea41fd2bbb3d195b8666b464b6a1844a02ccbb
fk2731 <leo.fk2731@gmail.com>
2025-09-16 23:01:07 -0600

    fix: jdtls on nvim

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ install_yay() {
 install_yay_packages() {
   yay -Syy -Syy --pgp-import --needed --noconfirm ttf-jetbrains-mono-nerd hyprshot cava telegram-desktop \
     swaync-git brave-bin rose-pine-cursor rose-pine-hyprcursor spotify grimblast python-psutil nemo \
-    python-setproctitle tesseract gpu-screen-recorder python-fabric-git celluloid jdk-openjdk || true
+    python-setproctitle tesseract gpu-screen-recorder python-fabric-git celluloid jdk-openjdk jdtls || true
 }
 
 setup_zsh() {
