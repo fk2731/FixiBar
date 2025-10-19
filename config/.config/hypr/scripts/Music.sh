9e9d14d9e7506e6765a56d345a337b0e2deb1bf8
fk2731 <leo.fk2731@gmail.com>
2025-10-18 23:54:22 -0600

    fix: Add application name

--- config/.config/hypr/scripts/Music.sh
+++ config/.config/hypr/scripts/Music.sh
@@ -4,17 +4,17 @@ move=false
 
 # If Spotify is already open, notify and exit
 if [[ -n $(hyprctl -j clients | jq -r '.[] | select(.class == "Spotify")') ]]; then
-  notify-send -u low "Already open" -i /usr/share/icons/hicolor/48x48/apps/spotify.png "Spotify open at workspace: $(hyprctl -j clients | jq -r '.[] | select(.class == "Spotify") | .workspace.id')" -t 2500
-  exit 0
+	notify-send -a "Fixi" -u low "Already open" -i /usr/share/icons/hicolor/48x48/apps/spotify.png "Spotify open at workspace: $(hyprctl -j clients | jq -r '.[] | select(.class == "Spotify") | .workspace.id')"
+	exit 0
 fi
 
 current_ws=$(hyprctl activeworkspace | grep 'ID' | awk 'NR==1 {print $3}')
 
 # Check if the current workspace is in use (has any client)
 if [[ -n $(hyprctl clients | grep "workspace" | grep "$current_ws") ]]; then
-  # Switch to an empty workspace
-  hyprctl dispatch workspace empty n+1 &
-  move=true
+	# Switch to an empty workspace
+	hyprctl dispatch workspace empty n+1 &
+	move=true
 fi
 
 # Launch Spotify in the (now) current workspace
